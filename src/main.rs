37934e3807a29007c6b23f48d412d12ab5db9760
Roland Neuber <rolandmartinneuber@gmail.com>
2024-11-20 15:54:09 +0100

    Add "OctPixel", braille variant of Multipixel

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use display::{Display, HexPixel, QuadPixel, SinglePixel};
+use display::{Display, HexPixel, OctPixel, QuadPixel, SinglePixel};
 use std::{thread, time};
 
 fn main() {
-    let mut disp: Display<QuadPixel> = Display::build_from_bools(
+    let mut disp: Display<OctPixel> = Display::build_from_bools(
         8, 
-        6, 
+        8, 
         vec![
+            true, false, true, false, true, false, true, false,
+            false, true, false, true, false, true, false, true,
             true, false, true, false, true, false, true, false,
             false, true, false, true, false, true, false, true,
             true, false, true, false, true, false, true, false,
@@ -17,12 +19,12 @@ fn main() {
 
     let duration = time::Duration::from_millis(100);
     for x in 0..8usize {
-        for y in 0..6usize {
+        for y in 0..8usize {
             thread::sleep(duration);
             disp.set_pixel(x, y, !disp.get_pixel(x, y).unwrap()).unwrap();
             println!("{}", disp.to_string());
-            print!("\x1b[3A");
+            print!("\x1b[2A");
         }
     }
-    print!("\x1b[3B");
+    print!("\x1b[2B");
 }
