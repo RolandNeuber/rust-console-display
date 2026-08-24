06a46a77a56ebbd8058e8213f42dae0f70bfd14f
Roland Neuber <rolandmartinneuber@gmail.com>
2026-08-24 12:38:02 +0200

    Remove rundant files. Add cranelift backend. Move to rust dev flake.

--- flake.nix
+++ flake.nix
@@ -3,56 +3,38 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    fenix.url = "github:nix-community/fenix";
+    rust-shell.url = "github:RolandNeuber/nix-config?dir=shells/rust";
+    cargo-coupling.url = "github:RolandNeuber/nix-config?dir=shells/cargo-coupling";
   };
 
-  outputs = { self, nixpkgs, fenix }:
+  outputs = { self, nixpkgs, fenix, rust-shell, cargo-coupling }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
-        overlays = [ fenix.overlays.default ];
-      };
-      toolchain = pkgs.fenix.fromToolchainFile {
-        file = ./rust-toolchain.toml;
-        sha256 = "sha256-VYaaz/Iz3wita53RiO4c00Orb/D5BQ9w66sjGd+QT+k=";
-      };
-      cargo-coupling = pkgs.rustPlatform.buildRustPackage {
-        pname = "cargo-coupling";
-        version = "v0.3.7";
-
-        nativeBuildInputs = with pkgs; [
-          git
-        ];
-        
-        src = pkgs.fetchFromGitHub {
-          owner = "nwiizo";
-          repo = "cargo-coupling";
-          rev = "main";
-          hash = "sha256-lIgtOVCgUiB319RwNF1G3UHCX7dl65F3VTnF8gLe8sE=";
-        };
-
-        cargoHash = "sha256-UQCneddJgqziw+vaSLKkaOC+hthjQzYyLWMZTsnsKVc=";
       };
     in {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          toolchain
+      devShells.${system}.default = rust-shell.lib.mkDevShell {
+        inherit system;
+        toolchainFile = ./rust-toolchain.toml;
+        toolchainHash = "sha256-VYaaz/Iz3wita53RiO4c00Orb/D5BQ9w66sjGd+QT+k=";
+        extraPackages = with pkgs; [
           pkg-config
-          ffmpeg
+          ffmpeg_8
 
           clang
           llvmPackages.libclang
 
           cargo-modules
           cargo-expand
-          cargo-coupling
+          cargo-coupling.packages.${system}.default
         ];
-
-        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-          pkgs.ffmpeg
-        ];
+        extraShellAttrs = {
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.ffmpeg
+          ];
+        };
       };
     };
 }
